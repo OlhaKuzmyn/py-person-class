66b6946162aa378c80db1dcda37722c593c6ec47
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-03-05 12:42:23 +0200

    solved

--- app/main.py
+++ app/main.py
@@ -1,26 +1,8 @@
 class Person:
-    people = {}
-
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        self.people[self.name] = self
+    # write your code here
+    pass
 
 
 def create_person_list(people: list) -> list:
-    for person in people:
-        person_obj = Person(person["name"], person["age"])
-        if "wife" in person and person["wife"] is not None:
-            person_obj.wife = person["wife"]
-        elif "husband" in person and person["husband"] is not None:
-            person_obj.husband = person["husband"]
-
-    for person_obj in Person.people.values():
-        for person_ in Person.people.values():
-            if (hasattr(person_, "wife")
-                    and person_.wife == person_obj.name):
-                person_.wife = person_obj
-            elif (hasattr(person_, "husband")
-                  and person_.husband == person_obj.name):
-                person_.husband = person_obj
-    return list(Person.people.values())
+    # write your code here
+    pass
